221fc36c9f61085c82fb39b9cd2a9995213c80da
Raul Ferreira <63669587+raul-ferreira@users.noreply.github.com>
2024-12-18 03:03:13 +0000

    Adicionar pasgina index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Teste 4</h1>
+    return <h1>Voce sabia que este site esta completamente online? <br></br> 
+    Isso quer dizer que qualquer pessoa com o link pode ver isso aqui. <br></br><br></br>
+    E assim, ela vai saber que: Barbara, eu te amo!!</h1>
 }
 
 export default Home;
