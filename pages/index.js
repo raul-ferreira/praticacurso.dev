021f9c285400a04ece7d6bbaa351190473891494
Raul Ferreira <63669587+raul-ferreira@users.noreply.github.com>
2026-01-25 17:54:12 +0000

    Atualizar mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
     return <h1>Voce sabia que este site esta completamente online? <br></br> 
-    Isso quer dizer que qualquer pessoa com o link pode ver isso aqui. <br></br><br></br>
-    E assim, ela vai saber que: Barbara, eu te amo!!</h1>
+    </h1>
 }
 
 export default Home;
